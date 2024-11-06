d4493e66bcb2656ea814a054013abbfe64d8683a
Samuel Moore <samoore@student.42london.com>
2024-11-06 16:07:10 +0000

    ci

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function MainScreen() {
   // const wsSource = 'http://127.0.0.1:5000/'
 
   
-
   const speakText = useCallback((message) => {
     console.log('Speaking text:', message);
     fetch(`${httpSource}speak_text`, {
