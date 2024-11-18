d035ab0e1f89ab06ba91ee66b8d809344cec74cf
Pedro pereira <pedroavpereira2000@gmail.com>
2024-11-18 12:24:47 +0000

    Add Link to websites logo

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import HamburgerSVG from "../HamburgerSVG/HamburgerSVG";
 import LogoSVG from "../LogoSVG/LogoSVG";
 
 function NavigationBar() {
   return (
     <div className="bg-hightlight h-16 flex justify-between items-center px-4">
-      <LogoSVG />
+      <Link to={""}>
+        <LogoSVG />
+      </Link>
       <div>
         {" "}
         <HamburgerSVG />
