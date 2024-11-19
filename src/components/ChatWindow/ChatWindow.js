4a55c167235c9ae9860120dbaae257d9429c20fc
Pedro pereira <pedroavpereira2000@gmail.com>
2024-11-19 14:30:51 +0000

    Add useSendAudioChat to ChatWindow button

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,7 +1,25 @@
-import { Mic, X } from "lucide-react";
+import { Mic, Octagon, X } from "lucide-react";
+import { useSendAudioChat } from "../../hooks/useSendAudioChat";
+import { Spinner } from "react-bootstrap";
 
 function ChatWindow({ onClose }) {
-  console.log("ChatWindow open");
+  const { isRecording, startRecording, stopRecording, isLoading } =
+    useSendAudioChat();
+
+  function handleMicrophoneClick() {
+    if (isLoading) {
+      return;
+    }
+
+    if (!isRecording) {
+      startRecording();
+      return;
+    } else {
+      stopRecording();
+      return;
+    }
+  }
+
   return (
     <div className="fixed bottom-0 w-full h-full bg-slate-200 px-8 py-4 flex flex-col justify-between">
       <div className="flex justify-between items-center">
@@ -12,8 +30,13 @@ function ChatWindow({ onClose }) {
       </div>
       <div className="flex-grow bg-white"></div>
       <div className="h-24 flex justify-center">
-        <button className="h-14 w-14 flex items-center justify-center bg-slate-300 rounded-full">
-          <Mic />
+        <button
+          className="h-14 w-14 flex items-center justify-center bg-slate-300 rounded-full"
+          onClick={handleMicrophoneClick}
+        >
+          {isLoading && <Spinner />}
+          {!isLoading && !isRecording && <Mic />}
+          {!isLoading && isRecording && <Octagon />}
         </button>
       </div>
     </div>
